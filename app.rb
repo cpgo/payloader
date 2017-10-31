caf9785bd1b396bb7035ce158f0af2acc6263a23
Cassio Godinho <cassio.godinho@zup.com.br>
2017-10-31 10:06:31 -0200

    change

--- app.rb
+++ app.rb
@@ -4,13 +4,12 @@ require "json"
 class App < Roda
   plugin :json
   route do |r|
-    r.root do
-      r.redirect "/post"
-    end
-    r.post do
-      response["Content-Type"] = "application/json"
-      puts "blablabl"
-      r.params.to_json
+    r.on "post" do
+      r.post do
+        response["Content-Type"] = "application/json"
+        puts "blablabl"
+        r.params.to_json
+      end
     end
   end
 end
