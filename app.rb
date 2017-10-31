1e19062a507732a73c2bfe34d1518fd17b4e1dfc
Cassio Godinho <cassio.godinho@zup.com.br>
2017-10-31 16:34:03 -0200

    new routes

--- app.rb
+++ app.rb
@@ -4,10 +4,20 @@ require "json"
 class App < Roda
   plugin :json
   route do |r|
+    r.root do
+      r.get do
+        "<h1> POST pra /post ou /post2</h1>"
+      end
+    end
     r.on "post" do
       r.post do
         response["Content-Type"] = "application/json"
-        puts "blablabl"
+        r.params.to_json
+      end
+    end
+    r.on "post2" do
+      r.post do
+        response["Content-Type"] = "application/json"
         r.params.to_json
       end
     end
